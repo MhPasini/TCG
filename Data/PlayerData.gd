d9e525bb12e84ddf131771ee227c72a4b19d224f
Matheus H. W. Pasini <151175485+MhPasini@users.noreply.github.com>
2024-09-03 21:53:03 -0300

    Play area drop
    reparent the card to the play area when played

--- Data/PlayerData.gd
+++ Data/PlayerData.gd
@@ -1,6 +1,8 @@
 extends Resource
 class_name PlayerDataClass
 
+const MAX_HEALTH = 2000
+
 var health : int = 2000
 var hand = [BaseCard]
 var deck = ['Ghoul', 'Cultist', 'MinorDemon', 'Cultist', 'Ghoul', 'MinorDemon']
